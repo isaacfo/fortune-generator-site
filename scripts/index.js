747bc395f7592437fc4c2ce146c8a2ae394365c9
Isaac Fonseca <isaacfo1010@gmail.com>
2018-10-11 09:47:04 -0400

    added button to queryselector

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 console.log("I'm here")
 
 // Make code run when I click button 
-
+const triggerElement = document.querySelector('[data-trigger]');
 
 // Write some text to the page on button click
 
 // Pull text from an array of text
-// to write to page
+// to write to pag
